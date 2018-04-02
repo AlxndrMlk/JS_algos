67d6e06988950717e7df6e9e2f9ad55e59bbaf0e
Aleksander Molak <aleksander.molak@gmail.com>
2018-04-02 10:31:26 +0200

    Update binSearch.js

--- binSearch.js
+++ binSearch.js
@@ -1,8 +1,8 @@
 const binSearch = function(array, x) {
   
-  // Takes an array and a number. 
-  // Returns an array with an index and number of steps performed to find the number in the array. 
-  // If number hasn't been found returns -1 as an index.
+  // Takes a sorted homogenous array of numbers or strings and a variable of the same type. 
+  // Returns an array with an index and number of steps performed to find the variable in the array. 
+  // If the variable hasn't been found returns -1 as an index.
   
   let min_val = 0
   let max_val = array.length - 1
